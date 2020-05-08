c86d3fdfc8ad0b7db75e8bcc2c06edb3d059bca1
Nicolas <npn.adsl@gmail.com>
2020-05-08 15:56:23 -0300

    Created OpenWeather tests file.

---
+++ Weather-BDDTests/OpenWatherForecastProviderTests.swift
@@ -0,0 +1,12 @@
+//
+//  OpenWatherForecastProviderTests.swift
+//  Weather-BDDTests
+//
+//  Created by Nicolas Nascimento on 08/05/20.
+//  Copyright © 2020 Nicolas Nascimento. All rights reserved.
+//
+
+import Foundation
+
+final class OpenWatherForecastProviderTests: QuickSpec {
+}
