7a736c9cc36d0dfeef2dc529ddc73311ef3811fb
Nicolas <npn.adsl@gmail.com>
2020-05-10 22:41:48 -0300

    Updated SceneDelegate to handle UITesting

--- Weather-BDD/Support/SceneDelegate.swift
+++ Weather-BDD/Support/SceneDelegate.swift
@@ -17,25 +17,35 @@ final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var state = AppState(status: .loadingForecast)
 
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-
-        // Create the SwiftUI view that provides the window contents.
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
         
+        // Create Interactor
         let apiKey = "YOUR_API_KEY"
         let provider = OpenWeatherForecastProvider(apiKey: apiKey, cityNames: ["San Francisco", "Porto Alegre"])
         let interactor = ForecastLoadingInteractor(forecastProvider: provider)
         
-        self.cancellable = interactor
-            .perform(action: .load, in: state)
-            .receive(on: RunLoop.main)
-            .sink(receiveCompletion: { _ in } ) { self.state.forecasts = $0 }
+        // Perform the request only if we are not testing
+        if CommandLine.arguments.contains("--UITESTING") {
+            let testingForecasts: [Forecast] = [
+                .init(cityName: "Porto Alegre", currentForecast: "Cloudy", currentTemp: 15, minTemp: 10, maxTemp: 20),
+                .init(cityName: "San Francisco", currentForecast: "Sunny", currentTemp: 20, minTemp: 15, maxTemp: 25)
+            ]
+            self.state.forecasts = testingForecasts
+            
+        } else {
+            self.cancellable = interactor
+                .perform(action: .load, in: state)
+                .receive(on: RunLoop.main)
+                .sink(receiveCompletion: { _ in } ) { self.state.forecasts = $0 }
+        }
         
+        // Share AppState among SwiftUI Views
         let contentView = ContentView()
             .environmentObject(state)
 
+        
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
