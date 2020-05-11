313ee0325e82ac26490bec98b602239d8a1bdff8
Nicolas <npn.adsl@gmail.com>
2020-05-10 22:11:22 -0300

    Modified ContentView to use AppState

--- Weather-BDD/View/ContentView.swift
+++ Weather-BDD/View/ContentView.swift
@@ -7,36 +7,36 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
-    
-    private let forecasts: [Forecast] = [
-        Forecast(cityName: "San Francisco", currentForecast: "Sunny", currentTemp: 20, minTemp: 15, maxTemp: 25),
-        Forecast(cityName: "Porto Alegre", currentForecast: "Cloudy", currentTemp: 15, minTemp: 10, maxTemp: 20)
-    ]
+    @EnvironmentObject var state: AppState
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(0..<forecasts.count) { index in
+                ForEach(self.state.forecasts) { forecast in
                     HStack {
                         VStack(alignment: .leading) {
-                            Text("\(self.forecasts[index].cityName) - \(Int(self.forecasts[index].currentTemp.rounded()))º")
+                            Text("\(forecast.cityName) - \(Int(forecast.currentTemp.rounded()))º")
                                 .font(.system(size: 17))
                                 .foregroundColor(Color.black)
-                            Text("\(Int(self.forecasts[index].minTemp.rounded()))º - \(Int(self.forecasts[index].maxTemp))º")
+                            Text("\(Int(forecast.minTemp.rounded()))º - \(Int(forecast.maxTemp))º")
                                 .font(.system(size: 15))
                                 .foregroundColor(Color.gray)
                         }
                         Spacer()
-                        Image(systemName: self.forecasts[index].currentForecast == "Sunny" ? "sun.max.fill" : "cloud.fill")
+                        Image(systemName: forecast.currentForecast == "Sunny" ? "sun.max.fill" : "cloud.fill")
                             .resizable()
                             .scaledToFit()
                             .frame(width: 38, height: 38, alignment: .center)
-                            
                     }
                 }
             }.navigationBarTitle("Forecasts")
         }
     }
+}
+
+extension Forecast: Identifiable {
+    var id: String { return cityName }
 }
